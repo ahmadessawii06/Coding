2065af45ec184abad8236d1f63e1cd2e94a8c47b
Ahmad Essawii. <ahmadessawii06@gmail.com>
2025-10-03 11:04:45 +0300

    Add Comments For PersonClass.cpp
    Adding A Comments For The Person Class

--- C++ Projects/Person Class/PersonClass.cpp
+++ C++ Projects/Person Class/PersonClass.cpp
@@ -1,43 +1,59 @@
-//! #include <ASE>
-#include <iostream>
-using namespace std;
+//! #include <ASE> // Improvment This is <ASE> Code :D
+#include <iostream> // Input and Output Library In C++.
+using namespace std; // To Use (cin) , (cout) , (endl) Without Typing std::...
 
+//! Class Person: (cls) To Show This Is A Class Not A Data Type Or Function
 class clsPerson
 {
-private:
-  int _ID;
-  string _FirstName;
-  string _LastName;
-  string _Email;
-  int _Phone;
+  // Private Attributes
+  private:
+    int _ID;                // To Store The Person's ID
+    string _FirstName;      // To Store The Perosn's FirstName
+    string _LastName;       // To Store The Person's LastName
+    string _Email;          // To Store The Person's Email
+    int _Phone;             // To Store The Person's Phone
 
-public:
-  clsPerson(int, string, string, string, int);
-  void setFirstName(string);
-  void setLastName(string);
-  void setEmail(string);
-  void setPhone(int);
-  int getID();
-  string getFirstName();
-  string getLastName();
-  string getFullName();
-  string getEmail();
-  int getPhone();
-  void printInfo();
-  void sendEmail(string, string);
-  void sendSMS(string);
+  // Public Methods
+  // Any Const Methods That's Mean I Don't Want To Modify The Data. Just Breaing It.
+  public:
+    clsPerson(int, string, string, string, int);     // A Constructor To Init The Values For Any Object (ID,FirstName,LastName,Email,Phone)
+    void setFirstName(string);                       // Setter Method To Set The FirstName For The Person
+    void setLastName(string);                        // Setter Method To Set The LastName For The Person
+    void setEmail(string);                           // Setter Method To Set The Email For The Person
+    void setPhone(int);                              // Setter Method To Set The Phone For The Person
+//  void setID();                                    // There Isn't Any Setter Method For The Person ID ( Due To I Don't Wan't To Applay The User Edit The ID)
+    int getID() const;                               // Const Getter Method To Get The ID For The Person  
+    string getFirstName() const;                     // Const Getter Method To Get The FirstName For The Person
+    string getLastName() const;                      // Const Getter Method To Get The LastName For The Person
+    string getFullName() const;                      // Const Getter Method To Get The FullName For The Person
+    string getEmail() const;                         // Const Getter Method To Get The Email For The Person
+    int getPhone() const;                            // Const Getter Method To Get The Phone For The Person
+    void printInfo() const;                          // Const Method To Print The Information For The Person Like: (ID,FirstName,LastName,FullName,Email,Phone)
+    void sendEmail(string, string);                  // Method To Send Email For The Person With (Subject,Body)
+    void sendSMS(string);                            // Method To Send SMS For The Person With (SMS)
 };
 
+// Implemantion For The Methods
 void clsPerson::setFirstName(string FirstName) { _FirstName = FirstName; };
+
 void clsPerson::setLastName(string LastName) { _LastName = LastName; };
+
 void clsPerson::setEmail(string Email) { _Email = Email; };
+
 void clsPerson::setPhone(int Phone) { _Phone = Phone; };
+
 int clsPerson::getID() { return _ID; };
+
 string clsPerson::getFirstName() { return _FirstName; };
+
 string clsPerson::getLastName() { return _LastName; };
+
 string clsPerson::getFullName() { return _FirstName + " " + _LastName; };
+
 string clsPerson::getEmail() { return _Email; };
+
 int clsPerson::getPhone() { return _Phone; };
+
 void clsPerson::printInfo()
 {
   cout << endl;
@@ -51,6 +67,7 @@ void clsPerson::printInfo()
   cout << "Phone       : " << getPhone() << endl;
   cout << "_______________________________" << endl;
 }
+
 void clsPerson::sendEmail(string Subject, string Body)
 {
   cout << "The Following Message Sent Successfully To Email: " << getEmail() << endl;
@@ -58,12 +75,14 @@ void clsPerson::sendEmail(string Subject, string Body)
   cout << "Body: " << Body << endl;
   cout << endl;
 }
+
 void clsPerson::sendSMS(string SMS)
 {
 
   cout << "The Following SMS Sent Successfully To Phone: " << getPhone() << endl;
   cout << SMS << endl;
 }
+
 clsPerson::clsPerson(int ID, string FirstName, string LastName, string Email, int Phone)
 {
   _ID = ID;
@@ -73,6 +92,7 @@ clsPerson::clsPerson(int ID, string FirstName, string LastName, string Email, in
   _Phone = Phone;
 }
 
+// Main Test
 int main()
 {
 
